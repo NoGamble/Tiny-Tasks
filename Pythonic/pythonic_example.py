f765400dbf8692cb10208ac4df67541c0b67deba
NoGamble <uestc520@gmail.com>
2025-09-11 08:50:14 +0800

    onnx

--- Pythonic/pythonic_example.py
+++ Pythonic/pythonic_example.py
@@ -4,4 +4,14 @@ for item in my_list:
 
 for num, item in enumerate(my_list):
     print(f"{num}:{item}")
-    
+
+
+
+# non Pythonic
+result = []
+for item in my_list:
+    if item not in result:
+        result.append(item)
+ 
+# Pythonic
+result = list(set(my_list))
